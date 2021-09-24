b43c4e68688aebb9b90677b938b63b2605b0f4ef
bobby3003 <roberto3003@riseup.net>
2021-09-24 22:22:26 +0700

    database

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
+use std::{collections::HashMap, error, fs::read_to_string};
 fn main() {
     let mut arguments = std::env::args().skip(1);
-    let key = arguments.next().unwrap();
+    let key = arguments.next().expect("Key was not there");
     let value = arguments.next().unwrap();
-    println!("The key is {}", key);
+    println!("The key is {}, and the value is {}", key, value);
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents).unwrap();
+
+    let database = Database::new().expect("Creating db failed");
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        // let contents = match std::fs::read_to_string("kv.db") {
+        //     Ok(c) => c,
+        //     Err(error) => {
+        //        return Err(error);
+        //     }
+        // };
+        let mut map = HashMap::new();
+        let contents = std::fs::read_to_string("kv.db")?;
+       for line in contents.lines() {
+       let mut chunks= line.splitn(2,'\t');
+       let key = chunks.next().expect("No key!");
+       let value = chunks.next().expect("No value!");
+       map.insert(key.to_owned(), value.to_string());
+       }
+
+        Ok(Database {
+            map: map
+        })
+    }
 }
