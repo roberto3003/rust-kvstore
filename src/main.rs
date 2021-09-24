2478470d660e6aaf7187dde30b27681352d6a1ff
bobby3003 <roberto3003@riseup.net>
2021-09-24 20:04:51 +0700

    added arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = std::env::args().skip(1);
+    let key = arguments.next().unwrap();
+    let value = arguments.next().unwrap();
+    println!("The key is {}", key);
 }
