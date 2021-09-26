113c204ed9c80c5b724500377efe9e195c929907
bobby3003 <roberto3003@riseup.net>
2021-09-26 20:50:16 +0700

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,60 @@
-use std::{collections::HashMap, error, fs::read_to_string};
+use std::collections::HashMap;
 fn main() {
     let mut arguments = std::env::args().skip(1);
     let key = arguments.next().expect("Key was not there");
     let value = arguments.next().unwrap();
     println!("The key is {}, and the value is {}", key, value);
-    let contents = format!("{}\t{}\n", key, value);
-    std::fs::write("kv.db", contents).unwrap();
 
-    let database = Database::new().expect("Creating db failed");
+    let mut database = Database::new().expect("Creating db failed");
+    database.insert(key.to_uppercase(), value.clone());
+    database.insert(key, value);
+    database.flush().unwrap();
 }
 
 struct Database {
     map: HashMap<String, String>,
+    flush: bool,
 }
 
 impl Database {
     fn new() -> Result<Database, std::io::Error> {
-        // let contents = match std::fs::read_to_string("kv.db") {
-        //     Ok(c) => c,
-        //     Err(error) => {
-        //        return Err(error);
-        //     }
-        // };
         let mut map = HashMap::new();
         let contents = std::fs::read_to_string("kv.db")?;
-       for line in contents.lines() {
-       let mut chunks= line.splitn(2,'\t');
-       let key = chunks.next().expect("No key!");
-       let value = chunks.next().expect("No value!");
-       map.insert(key.to_owned(), value.to_string());
-       }
+        for line in contents.lines() {
+            let mut chunks = line.splitn(2, '\t');
+            let key = chunks.next().expect("No key!");
+            let value = chunks.next().expect("No value!");
+            map.insert(key.to_owned(), value.to_string());
+        }
 
-        Ok(Database {
-            map: map
-        })
+        Ok(Database { map, flush: false })
     }
+
+    fn insert(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
+    }
+
+    fn flush(mut self) -> std::io::Result<()> {
+        self.flush = true;
+        do_flush(&self)
+    }
+}
+
+impl Drop for Database {
+    fn drop(&mut self) {
+        if !self.flush {
+            let _ = do_flush(self);
+        }
+    }
+}
+
+fn do_flush(database: &Database) -> std::io::Result<()> {
+    let mut contents = String::new();
+    for (key, value) in &database.map {
+        contents.push_str(key);
+        contents.push('\t');
+        contents.push_str(value);
+        contents.push('\n');
+    }
+    std::fs::write("kv.db", contents)
 }
